d65a9168dfffcfee40ab721a0aade339a37355fa
Pildonich <mr_divis@mail.ru>
2018-06-27 14:26:26 +0700

    Рефакторинг

--- js/stat.js
+++ js/stat.js
@@ -10,20 +10,14 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_SPACE = 50;
 
-var renderCloud = function (ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, width, height);
 };
 
 var renderText = function (ctx, text, x, y, font, color) {
-  if (font === undefined) {
-    font = '16px PT Mono';
-  }
-  ctx.font = font;
-  if (color === undefined) {
-    color = '#000000';
-  }
-  ctx.fillStyle = color;
+  ctx.font = font || '16px PT Mono';
+  ctx.fillStyle = color || '#000000';
   ctx.textBaseline = 'hanging';
   ctx.fillText(text, x, y);
 };
@@ -41,8 +35,8 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
   renderText(ctx, 'Ура вы победили!', 120, 30);
   renderText(ctx, 'Список результатов:', 120, 50);
 
@@ -53,11 +47,7 @@ window.renderStatistics = function (ctx, names, times) {
     var ratioBar = (BAR_HEIGHT * times[i]) / maxTime;
     renderText(ctx, names[i], BAR_X, BAR_Y + GAP);
     renderText(ctx, Math.round(times[i]), BAR_X, BAR_Y - ratioBar - GAP * 2);
-    ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    }
-    ctx.fillRect(BAR_X, BAR_Y, BAR_WIDTH, ratioBar * -1);
+    renderCloud(ctx, BAR_X, BAR_Y, BAR_WIDTH, ratioBar * -1, (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')');
   }
 };
 
